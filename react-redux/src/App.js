f68b9d7c72ea68da7c4a93ea3467c432a22707c6
科匠 <zhoubingqian@wondersgroup.com>
2019-01-05 17:35:32 +0800

    aa

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -2,14 +2,77 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      input1: ['button10'],
+      input2: [
+        {
+          name: "button20",
+          value: "",
+          list: []
+        }
+      ]
+    }
+  }
+
+  // 主按钮添加事件
+  add = () => {
+    let index = this.state.input1.length;
+    this.state.input1.push('button1'+index);
+    this.state.input2.push({name: "button2"+index,value: "",list:[]});
+    console.log(this.state.input1);
+    this.setState({
+      input1: this.state.input1,
+      input2: this.state.input2
+    })
+  }
+
+  // 主list
+  list1=(item)=>{
+    let dom = [];
+    item.forEach((val,index) => {
+      dom.push(
+        <button key={index}>{val}</button>
+      )
+    })
+    return dom;
+  }
+
+  // 副list
+  list2=(item)=>{
+    let dom = [];
+    item.forEach((val,key) => {
+      dom.push(
+        <div>
+          <button key={key} onClick={this.child.bind(this,key,val)}>{val.name}</button>
+          {val.list.map(( item ) => {
+            return <button>{item.name}</button>
+          })}
+        </div>
+      )
+    })
+    return dom;
+  }
+
+  // 副list添加子组件
+  child = (e,index,val) => {
+    var num = this.state.input2[e].list.length + 1;
+    this.state.input2[e].list.push({name: "button2"+e+'-'+num,value: ""});
+    console.log(this.state.input2)
+    this.setState({
+      input2: this.state.input2
+    })
+  }
+  
   render() {
     return (
       <div className="container">
-        <h1 className="jumbotron-heading text-center">1</h1>
-        <p className="text-center">
-          <button className="btn btn-primary mr-2">Increase</button>
-          <button className="btn btn-danger my-2">Decrease</button>
-        </p>
+        <button onClick={this.add}>添加</button>
+        <p>这是横</p>
+        {this.list1(this.state.input1)}
+        <p>这是竖</p>
+        {this.list2(this.state.input2)}
       </div>
     );
   }
