7f04c607e5ba617d33509343b3f1f758dfd17d21
branton <937935598@qq.com>
2019-03-12 10:50:48 +0800

    安装修饰符

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,39 +1,28 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { extendObservable } from "mobx";
-import { observer } from "mobx-react";
 
 class App extends Component {
-  constructor(){
-    super();
-    extendObservable(this,{
-      counter: 0
-    })
-  }
-  componentWillMount(){
-    
-  }
-
-  onIncrement = () => {
-    this.counter++;
-  }
-
-  onDecrement = () => {
-    this.counter--;
-  }
-
   render() {
-    console.log(this.counter);
     return (
-      <div className="container">
-        <h1 className="jumbotron-heading text-center">{this.counter}</h1>
-        <p className="text-center">
-          <button onClick={this.onIncrement} className="btn btn-primary mr-2">加1</button>
-          <button onClick={this.onDecrement} className="btn btn-danger my-2">减1</button>
-        </p>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
 }
 
-export default observer(App);
+export default App;
